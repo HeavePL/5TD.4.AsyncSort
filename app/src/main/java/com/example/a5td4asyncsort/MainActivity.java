fab6babcb875541f0e72727483245a2840e0fcd8
Bartek <tomek.u102@gmail.com>
2024-11-12 16:33:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a5td4asyncsort/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.a5td4asyncsort;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExecutorService executorService;
+    Handler handler;
+    ProgressBar progressBar;
+    int[] array;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        EditText editTextNumber = findViewById(R.id.editTextNumber);
+        Button startBtn = findViewById(R.id.btnStart);
+
+        executorService = Executors.newSingleThreadExecutor();
+        handler = new Handler(Looper.getMainLooper());
+
+        startBtn.setOnClickListener(v -> {
+            String sizeStr = editTextNumber.getText().toString();
+                if (sizeStr.isEmpty()){
+                    Toast.makeText(this, "Wprowadz ilosc elementow", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            int size = Integer.parseInt(sizeStr);
+            array = generateArray(size);
+            progressBar.setProgress(0);
+
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    bubbleSort();
+                }
+            });
+
+
+
+
+        });
+
+
+
+
+    }
+
+    private int[] generateArray(int size){
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(150) + 1;
+        }
+        return array;
+    }
+
+    private void bubbleSort(){
+        if(array.length<=1){
+            handler.post(()->{
+
+                progressBar.setProgress(100);
+                Toast.makeText(this,"Sorting completed", Toast.LENGTH_SHORT).show();
+            });
+            return;
+        }
+
+        int n = array.length;
+        int temp = 0;
+        for(int i=0; i < (n - 1); i++){
+            for(int j=0; j < ( n- i - 1); j++){
+                if(array[j] > array[j + 1]){
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    // i * n => full cycles + j => partial cycle
+                    int progress =  (int) (((float) (i * n + j) / (n*n))*100);
+                    handler.post(()->{
+                        progressBar.setProgress(progress);
+                    });
+
+                    try {
+                        Thread.sleep(6);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+        handler.post(()->{
+            progressBar.setProgress(100);
+            Toast.makeText(this,"Sorting completed", Toast.LENGTH_SHORT).show();
+        });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/a5td4asyncsort/SortView.java
@@ -0,0 +1,15 @@
+package com.example.a5td4asyncsort;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SortView extends View {
+
+    public SortView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
